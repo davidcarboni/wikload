938530dfd44732a00b379a92858f031d521fc0d3
David Carboni <david@carboni.io>
2019-08-12 09:50:05 +0100

    Basic auth and govuk-frontend support.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, Markup, render_template, abort
+from flask import Flask, Markup, render_template, url_for, send_from_directory, send_file, abort
+from flask_basicauth import BasicAuth
 import markdown2
 import os
 
@@ -10,20 +11,29 @@ app = Flask(__name__)
 username = os.getenv('USERNAME')
 password = os.getenv('PASSWORD')
 if username and password:
+    print(f"Setting up authentication for user {username}")
+    basic_auth = BasicAuth(app) 
     app.config['BASIC_AUTH_USERNAME'] = username
     app.config['BASIC_AUTH_PASSWORD'] = password
     app.config['BASIC_AUTH_FORCE'] = True
+else:
+    print(f"Not setting up authentication. USERNAME: {username}, PASSWORD: {password != ''}")
 
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
+#@basic_auth.required
 def catch_all(path):
     """ Catch-all route 
 
     Renders markdown for the requested path, 
     if a corresponding .md file can be found.
+
+    TODO: glaring use of user-supplied path here
     """
-    print('You want path: %s' % path)
+    print(f'Rendering path: {path}')
+
+    # Locate markdown
     markdown = path.strip('/') + '.md'
     if not path:
         # Github wiki home page
@@ -32,9 +42,27 @@ def catch_all(path):
         # Consider implementing case-insensive match. Maybe.
         print(f'{markdown} not found.')
         abort(404)
+    
+    # Render content
     markdown = f'./wiki/{markdown}'
     html = markdown2.markdown_path(markdown)
     return render_template('page.html', content=Markup(html))
+
+@app.route('/stylesheets/<path:path>')
+@app.route('/javascript/<path:path>')
+def govuk_frontend_cssjs(path):
+    """ Static govuk-frontend content: css and js."""
+    return send_from_directory('govuk-frontend/dist', path)
+
+@app.route('/assets/<path:path>')
+def govuk_frontend_assets(path):
+    """ Static govuk-frontend content: fonts and images."""
+    return send_from_directory('govuk-frontend/dist/assets', path)
+
+@app.route('/favicon.ico')
+def favicon():
+    """ Static govuk-frontend content: favicon.ico."""
+    return send_file('govuk-frontend/dist/assets/images/favicon.ico')
 
 # Run the app (if this file is called directly, not through 'flask run')
 # This is isn't recommended, but it's enough to run a low-traffic wiki
