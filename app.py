fb30857343d25029b338dd534d264cd7db4fd2b0
David Carboni <david@carboni.io>
2019-08-13 14:25:44 +0100

    Govuk frontend styling.

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ def home():
     return redirect("/wiki")
 
 @app.route('/wiki', defaults={'path': ''})
+@app.route('/wiki/', defaults={'path': ''})
 @app.route('/wiki/<path:path>')
 def catch_all(path):
     """ Catch-all route 
@@ -51,20 +52,40 @@ def catch_all(path):
     # Render content
     markdown = f'./wiki/{markdown}.md'
     html = markdown2.markdown_path(markdown)
-    return render_template('page.html', content=Markup(html))
+    html = style(html)
+    nav = markdown2.markdown_path('./wiki/_Sidebar.md')
+    print(nav)
+    nav = style(nav)
+    return render_template('page.html', content=Markup(html), nav=Markup(nav))
+
+def style(html):
+    styled = html
+    styled = styled.replace('<h1>', '<h1 class="govuk-heading-l">')
+    styled = styled.replace('<h2>', '<h2 class="govuk-heading-m">')
+    styled = styled.replace('<h3>', '<h3 class="govuk-heading-s">')
+    styled = styled.replace('<h4>', '<h3 class="govuk-heading-xs">')
+    styled = styled.replace('<p>', '<p class="govuk-body">')
+    styled = styled.replace('<a ', '<a class="govuk-link" ')
+    styled = styled.replace('<ul>', '<ul class="govuk-list govuk-!-font-size-16">')
+    styled = styled.replace('<li>', '<li class="gem-c-related-navigation__link">')
+    return styled
 
 @app.route('/stylesheets/<path:path>')
 @app.route('/javascript/<path:path>')
+@app.route('/wiki/stylesheets/<path:path>')
+@app.route('/wiki/javascript/<path:path>')
 def govuk_frontend_cssjs(path):
     """ css / js."""
     return send_from_directory('govuk-frontend/dist', path)
 
 @app.route('/assets/<path:path>')
+@app.route('/wiki/assets/<path:path>')
 def govuk_frontend_assets(path):
     """ Fonts and images."""
     return send_from_directory('govuk-frontend/dist/assets', path)
 
 @app.route('/favicon.ico')
+@app.route('/wiki/favicon.ico')
 def favicon():
     """ favicon.ico."""
     return send_file('govuk-frontend/dist/assets/images/favicon.ico')
