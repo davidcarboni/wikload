fcd6eec7c1bad74b86226030931daad9de9a35ef
David Carboni <david@carboni.io>
2019-12-20 17:06:26 +0000

    Updated the run script.

--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+export GITHUB_USERNAME=davidcarboni
+export GITHUB_ACCESS_TOKEN=$(cat credentials/token.txt)
+export GITHUB_REPO=carboni/carboni.github.io
+
 # Run as a container for local test/development
-docker build --tag govwiki . && \
-docker run -it --rm -e USERNAME=wiki -e PASSWORD=123 -p 5000:5000 govwiki
+docker build --tag gikki . && \
+docker run -it --rm -p 5000:5000 \
+    -e USERNAME=wiki \
+    -e PASSWORD=123 \
+    -e NOSSL=true \
+    -e GITHUB_USERNAME \
+    -e GITHUB_ACCESS_TOKEN \
+    -e GITHUB_REPO \
+    gikki
